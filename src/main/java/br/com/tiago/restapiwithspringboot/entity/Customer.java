f88d8cefda91fd5d17b6aad4c7b15d0dde325993
Jessica Graciele <jessicagraciele.cb@gmail.com>
2023-03-28 19:36:31 -0300

    primeira

--- src/main/java/br/com/tiago/restapiwithspringboot/entity/Customer.java
+++ src/main/java/br/com/tiago/restapiwithspringboot/entity/Customer.java
@@ -1,19 +1,38 @@
 package br.com.tiago.restapiwithspringboot.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
+@Data
 public class Customer {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_customer")
     private Long idCustomer;
+    @Column(name = "first_name_customer", nullable = false, length = 50)
     private String firstNameCustomer;
+    @Column(name = "last_name_customer", nullable = false, length = 50)
     private String lastNameCustomer;
+    @Column(name = "cpf_customer", nullable = false, length = 11)
     private String cpfCustomer;
+    @Column(name = "birth_date_customer", nullable = false, updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate birthdateCustomer;
+    @Column(name = "date_created_customer", nullable = false, updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dateCreatedCustomer;
+    @Column(name = "monthly_income_customer", nullable = false, precision = 10, scale = 2)
     private BigDecimal monthlyIncomeCustomer;
     private Boolean statusCustomer;
+    @Column(name = "email_customer", nullable = false, length = 100)
     private String emailCustomer;
+    @Column(name = "password_customer", nullable = false, length = 16)
     private String passwordCustomer;
+
+    @PrePersist
+    public void prePersist(){ setDateCreatedCustomer(LocalDate.now());}
 
 }
