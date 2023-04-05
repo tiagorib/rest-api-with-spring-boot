e8b5f65936bc1db2d4d0a9e2a90839a77007fa84
unknown <ronaldo__mj@hotmail.com>
2023-04-04 21:48:25 -0300

    trabalho 1

--- src/main/java/br/com/tiago/restapiwithspringboot/controller/CustomerController.java
+++ src/main/java/br/com/tiago/restapiwithspringboot/controller/CustomerController.java
@@ -1,18 +1,17 @@
 package br.com.tiago.restapiwithspringboot.controller;
 
 import br.com.tiago.restapiwithspringboot.entity.Customer;
+import br.com.tiago.restapiwithspringboot.entity.Product;
 import br.com.tiago.restapiwithspringboot.exception.ResponseGenericException;
 import br.com.tiago.restapiwithspringboot.service.CustomerService;
+import br.com.tiago.restapiwithspringboot.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1/customer")
@@ -22,13 +21,16 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-
+    @GetMapping(value = "/list")
+    public ResponseEntity<Object> getInfoCustomers() {
+        List<Customer> result = customerService.getInfoCustomers();
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
     @PostMapping(value = "/create")
     public ResponseEntity<Object> saveCustomer(@RequestBody Customer customer) {
         Customer result = customerService.saveCustomer(customer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
-
 
     @DeleteMapping(value = "/delete/{idCustomer}")
     public ResponseEntity<Object> deleteCustomer(@PathVariable Long idCustomer) {
@@ -38,7 +40,7 @@ public class CustomerController {
 
     @GetMapping(value = "/findCustomer/{idCustomer}")
     public ResponseEntity<Object> getCustomerById(@PathVariable Long idCustomer){
-        Customer result = customerService.findCustomerById(idCustomer);
+        Optional<Customer> result = customerService.findCustomerById(idCustomer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
