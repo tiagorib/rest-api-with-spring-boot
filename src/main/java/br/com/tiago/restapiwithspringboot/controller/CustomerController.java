29fece54ffd0a24fda3ba95e9606a0a12489eaf8
Vitor Militão <vmilitao1945@gmail.com>
2023-04-10 22:19:59 -0300

    Just some operations using Swagger

--- src/main/java/br/com/tiago/restapiwithspringboot/controller/CustomerController.java
+++ src/main/java/br/com/tiago/restapiwithspringboot/controller/CustomerController.java
@@ -5,6 +5,7 @@ import br.com.tiago.restapiwithspringboot.entity.Product;
 import br.com.tiago.restapiwithspringboot.exception.ResponseGenericException;
 import br.com.tiago.restapiwithspringboot.service.CustomerService;
 import br.com.tiago.restapiwithspringboot.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,23 +23,27 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping(value = "/list")
+    @Operation(summary = "This method lists all customers")
     public ResponseEntity<Object> getInfoCustomers() {
         List<Customer> result = customerService.getInfoCustomers();
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
     @PostMapping(value = "/create")
+    @Operation(summary = "This method creates a new customer")
     public ResponseEntity<Object> saveCustomer(@RequestBody Customer customer) {
         Customer result = customerService.saveCustomer(customer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @DeleteMapping(value = "/delete/{idCustomer}")
+    @Operation(summary = "This method deletes a customer")
     public ResponseEntity<Object> deleteCustomer(@PathVariable Long idCustomer) {
         HashMap<String, Object> result = customerService.deleteCustomer(idCustomer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @GetMapping(value = "/findCustomer/{idCustomer}")
+    @Operation(summary = "This method is used to find a customer")
     public ResponseEntity<Object> getCustomerById(@PathVariable Long idCustomer){
         Optional<Customer> result = customerService.findCustomerById(idCustomer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
@@ -46,6 +51,7 @@ public class CustomerController {
 
 
     @PutMapping(value = "/update")
+    @Operation(summary = "This method is used to update a customer")
     public ResponseEntity<Object> updateCustomer(@RequestBody Customer customer) {
         Customer result = customerService.updateCustomer(customer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
