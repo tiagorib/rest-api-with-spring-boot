586662aa8b65353d19a32f2edbbc33e2dc672ddb
rafael corturato <rafael.corturato@fatec.sp.gov.br>
2023-04-11 21:11:33 -0300

    add swagger

--- src/main/java/br/com/tiago/restapiwithspringboot/RestApiWithSpringBootApplication.java
+++ src/main/java/br/com/tiago/restapiwithspringboot/RestApiWithSpringBootApplication.java
@@ -1,10 +1,13 @@
 package br.com.tiago.restapiwithspringboot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@OpenAPIDefinition(info=@Info(title="REST API Fatec Jales",description = "REST API Fatec SpringBoot"))
 public class RestApiWithSpringBootApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/tiago/restapiwithspringboot/controller/CustomerController.java
+++ src/main/java/br/com/tiago/restapiwithspringboot/controller/CustomerController.java
@@ -3,6 +3,7 @@ package br.com.tiago.restapiwithspringboot.controller;
 import br.com.tiago.restapiwithspringboot.entity.Customer;
 import br.com.tiago.restapiwithspringboot.exception.ResponseGenericException;
 import br.com.tiago.restapiwithspringboot.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,34 +20,40 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping(value = "/list")
+    @Operation(summary = "Method for list all Customers")
     public ResponseEntity<Object> getInfoCustomer() {
         List<Customer> result = customerService.getInfoCustomer();
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
     @PostMapping(value = "/create")
+    @Operation(summary = "Method for create Customer")
     public ResponseEntity<Object> saveCustomer(@RequestBody Customer customer) {
         Customer result = customerService.saveCustomer(customer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @DeleteMapping(value = "/delete/{idCustomer}")
+    @Operation(summary = "Method for delete Customer")
     public ResponseEntity<Object> deleteCustomer(@PathVariable Long idCustomer) {
         HashMap<String, Object> result = customerService.deleteCustomer(idCustomer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
-    @GetMapping(value = "/findProduct/{idCustomer}")
+    @GetMapping(value = "/findCustomer/{idCustomer}")
+    @Operation(summary = "Method for find Customer by ID")
     public ResponseEntity<Object> getCustomerById(@PathVariable Long idCustomer){
         Customer result = customerService.findCustomerById(idCustomer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
     @GetMapping(value = "/listCustomer/{statusCustomer}")
+    @Operation(summary = "Method for list Customer with status true")
     public ResponseEntity<Object> findAllCustomerByStatus(@PathVariable Boolean statusCustomer){
         List<Customer> result = customerService.findAllCustomersByStatus(statusCustomer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
     @PutMapping(value = "/update")
+    @Operation(summary = "Method for update Customer")
     public ResponseEntity<Object> updateCustomer(@RequestBody Customer customer) {
         Customer result = customerService.updateProduct(customer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
