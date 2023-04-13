875c3a0c0187da2e17e38c15e04baa51561070a0
Mike André <mkekamilo@gmail.com>
2023-04-12 21:58:47 -0300

    feat: implements swagger

--- src/main/java/br/com/tiago/restapiwithspringboot/controller/CustomerController.java
+++ src/main/java/br/com/tiago/restapiwithspringboot/controller/CustomerController.java
@@ -1,18 +1,18 @@
 package br.com.tiago.restapiwithspringboot.controller;
 
 import br.com.tiago.restapiwithspringboot.entity.Customer;
+import br.com.tiago.restapiwithspringboot.entity.Product;
 import br.com.tiago.restapiwithspringboot.exception.ResponseGenericException;
 import br.com.tiago.restapiwithspringboot.service.CustomerService;
+import br.com.tiago.restapiwithspringboot.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/v1/customer")
@@ -22,13 +22,17 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-
+    @GetMapping(value = "/list")
+    @Operation(summary = "List all the customer")
+    public ResponseEntity<Object> getInfoCustomers() {
+        List<Customer> result = customerService.getInfoCustomers();
+        return ResponseEntity.ok().body(ResponseGenericException.response(result));
+    }
     @PostMapping(value = "/create")
     public ResponseEntity<Object> saveCustomer(@RequestBody Customer customer) {
         Customer result = customerService.saveCustomer(customer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
-
 
     @DeleteMapping(value = "/delete/{idCustomer}")
     public ResponseEntity<Object> deleteCustomer(@PathVariable Long idCustomer) {
@@ -38,7 +42,7 @@ public class CustomerController {
 
     @GetMapping(value = "/findCustomer/{idCustomer}")
     public ResponseEntity<Object> getCustomerById(@PathVariable Long idCustomer){
-        Customer result = customerService.findCustomerById(idCustomer);
+        Optional<Customer> result = customerService.findCustomerById(idCustomer);
         return ResponseEntity.ok().body(ResponseGenericException.response(result));
     }
 
--- src/main/java/br/com/tiago/restapiwithspringboot/service/CustomerService.java
+++ src/main/java/br/com/tiago/restapiwithspringboot/service/CustomerService.java
@@ -4,7 +4,7 @@ import br.com.tiago.restapiwithspringboot.entity.Customer;
 import br.com.tiago.restapiwithspringboot.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -19,71 +19,78 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public List<Customer> getInfoCustomer() {
+    public List<Customer> getInfoCustomers() {
         return customerRepository.findAll();
     }
 
     public Customer saveCustomer(Customer customer) {
         if (validateCustomer(customer)) {
+            encryptPassword(customer);
             return customerRepository.saveAndFlush(customer);
         } else {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Não foi encontrado os dados dos clientes)!");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "O preço de venda do produto é " +
+                            "obrigatório e deve ser maior que 0 (zero)!");
         }
     }
 
-    public HashMap<String, Object> deleteCustomer(Long customerId) {
+    public HashMap<String, Object> deleteCustomer(Long idCustomer) {
         Optional<Customer> customer =
-                Optional.ofNullable(customerRepository.findById(customerId).
+                Optional.ofNullable(customerRepository.findById(idCustomer).
                         orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                                "Cliente não encontrado!")));
+                                "Produto não encontrado!")));
+
 
         customerRepository.delete(customer.get());
-        HashMap<String, Object> result = new  HashMap<String, Object> ();
-        result.put("result", "Cliente: " + customer.get().getFirstNameCustomer() + " excluído com sucesso!");
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("result", "Cliente: " + customer.get().getFirstNameCustomer() + " " + customer.get().getLastNameCustomer() +  " excluído com sucesso!");
         return result;
     }
 
-    public Customer findCustomerById(Long idCustomer){
-        return customerRepository.findById(idCustomer)
-                .orElseThrow(() ->  new ResponseStatusException(HttpStatus.NOT_FOUND,
-                        "Cliente não encontrado!"));
+    public Optional<Customer> findCustomerById(Long idCustomer) {
+        return Optional.ofNullable(customerRepository.findById(idCustomer)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Cliente não encontrado!")));
     }
 
     public Customer updateCustomer(Customer customer) {
-
-        if (customer.getIdCustomer() == null || customer.getIdCustomer() <= 0) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                    "O ID do cliente é obrigatório na atualização!");
-        }
-
         if (validateCustomer(customer)) {
             if (findCustomerById(customer.getIdCustomer()) != null) {
+                encryptPassword(customer);
                 return customerRepository.saveAndFlush(customer);
             } else {
                 return null;
             }
         } else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    "O valor que o cliente deverá pagar " +
-                            " devem ser maiores que 0 (zero)!");
+                    "A renda salarial deve ser maior ou igual 0!");
         }
     }
 
-    public Boolean validateCustomer (Customer customer) {
-        if (customer.getEmailCustomer() != null &&
-                customer.getCpfCustomer()  != null &&
-                customer.getStatusCustomer()  != null &&
-                customer.getBirthdateCustomer()  != null &&
-                customer.getPasswordCustomer()  != null &&
-                customer.getFirstNameCustomer()  != null &&
-                customer.getLastNameCustomer()  != null &&
-                customer.getMonthlyIncomeCustomer()  != null &&
-                customer.getDateCreatedCustomer()  != null
-        ) {
+    public Boolean validateCustomer(Customer customer) {
+        if (customer.getMonthlyIncomeCustomer() != null &&
+                customer.getMonthlyIncomeCustomer().compareTo(BigDecimal.valueOf(0)) >= 0 &&
+                customer.getPasswordCustomer() != null &&
+                !customer.getPasswordCustomer().equals("")) {
             return true;
         } else {
             return false;
         }
     }
 
+    public void encryptPassword(Customer customer){
+        BCryptPasswordEncoder encrypt = new BCryptPasswordEncoder();
+        String encryptedPassword = null;
+        if (customer.getIdCustomer() == null) {
+            encryptedPassword = encrypt.encode(customer.getPasswordCustomer());
+            customer.setPasswordCustomer(encryptedPassword);
+        } else {
+            if (!customerRepository.findById(customer.getIdCustomer()).get().getPasswordCustomer()
+                    .equals(customer.getPasswordCustomer())) {
+                encryptedPassword = encrypt.encode(customer.getPasswordCustomer());
+                customer.setPasswordCustomer(encryptedPassword);
+            }
+        }
+
+    }
 }
